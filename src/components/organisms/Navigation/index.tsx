3b6f523232e5d281564eae9db3b5b5fd5e52c259
kukv <rio.by.kukv@gmail.com>
2023-12-31 16:46:45 +0900

    SectionLinkを使う

--- src/components/organisms/Navigation/index.tsx
+++ src/components/organisms/Navigation/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationItem } from '@/components/atoms/NavigationItem'
 import { NavigationLogo } from '@/components/molecules/NavigationLogo'
+import { SectionLink } from '@/components/molecules/SectionLinks'
 import { useMobileMenuStore } from '@/stores/MobileMenuStore'
 import { CollapsedMenuButton } from 'src/components/molecules/CollapsedMenuButton'
 
@@ -18,12 +19,7 @@ export const Navigation = () => {
         />
       </div>
       <div className="hidden lg:flex lg:gap-x-12">
-        <NavigationItem title="Home" url="#home" mobile={false} />
-        <NavigationItem title="About" url="#about" mobile={false} />
-        <NavigationItem title="Resume" url="#resume" mobile={false} />
-        <NavigationItem title="Skills" url="#skills" mobile={false} />
-        <NavigationItem title="Works" url="#works" mobile={false} />
-        <NavigationItem title="Contact" url="#contact" mobile={false} />
+        <SectionLink isMobile={false} />
       </div>
       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
         <NavigationItem title="Blog &rarr;" url="#" mobile={false} />
