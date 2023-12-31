f9c28e25a18907bc62cb1f19fe6ce27c1cd216d7
kukv <rio.by.kukv@gmail.com>
2023-12-31 16:21:08 +0900

    Popover.Groupをやめてdiv要素に変更 スタイルを調整

--- src/components/organisms/Navigation/index.tsx
+++ src/components/organisms/Navigation/index.tsx
@@ -1,14 +1,13 @@
 import { NavigationItem } from '@/components/atoms/NavigationItem'
 import { NavigationLogo } from '@/components/molecules/NavigationLogo'
 import { useMobileMenuStore } from '@/stores/MobileMenuStore'
-import { Popover } from '@headlessui/react'
 import { CollapsedMenuButton } from 'src/components/molecules/CollapsedMenuButton'
 
 export const Navigation = () => {
   const { open } = useMobileMenuStore()
 
   return (
-    <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+    <nav className="flex flex-row mx-auto max-w-7xl items-center justify-between p-6 lg:px-8">
       <div className="flex lg:flex-1">
         <NavigationLogo />
       </div>
@@ -18,14 +17,14 @@ export const Navigation = () => {
           action={() => open()}
         />
       </div>
-      <Popover.Group className="hidden lg:flex lg:gap-x-12">
+      <div className="hidden lg:flex lg:gap-x-12">
         <NavigationItem title="Home" url="#home" mobile={false} />
         <NavigationItem title="About" url="#about" mobile={false} />
         <NavigationItem title="Resume" url="#resume" mobile={false} />
         <NavigationItem title="Skills" url="#skills" mobile={false} />
         <NavigationItem title="Works" url="#works" mobile={false} />
         <NavigationItem title="Contact" url="#contact" mobile={false} />
-      </Popover.Group>
+      </div>
       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
         <NavigationItem title="Blog &rarr;" url="#" mobile={false} />
       </div>
