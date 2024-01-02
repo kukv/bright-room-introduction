cbc0cf1ab4c2446d61ef46344101024008e38d7a
kukv <rio.by.kukv@gmail.com>
2024-01-03 05:15:07 +0900

    SideMenuのStorybookを追加

---
+++ src/components/organisms/SideMenu/index.stories.ts
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SideMenu } from '.'
+
+const meta = {
+  title: 'organisms/SideMenu',
+  component: SideMenu,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof SideMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Template: Story = {
+  args: {
+    isOpen: true,
+    closingAction: () => 1,
+  },
+}
