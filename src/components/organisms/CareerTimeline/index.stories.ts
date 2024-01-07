c30c0178666dcec69be72df2210669d26cbf2f66
kukv <rio.by.kukv@gmail.com>
2024-01-08 07:22:25 +0900

    キャリアタイムラインを分離

---
+++ src/components/organisms/CareerTimeline/index.stories.ts
@@ -0,0 +1,61 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { CareerTimeline } from '.'
+
+const meta = {
+  title: 'organisms/CareerTimeline',
+  component: CareerTimeline,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof CareerTimeline>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Template: Story = {
+  args: {
+    careers: [
+      {
+        year: 2024,
+        items: [
+          {
+            month: 'Mar',
+            day: 1,
+            title: 'Joined ??? Inc.',
+            description: '内容',
+          },
+        ],
+      },
+      {
+        year: 2022,
+        items: [
+          {
+            month: 'May',
+            day: 2,
+            title: 'Joined ??? Inc.',
+            description: '内容',
+          },
+        ],
+      },
+      {
+        year: 2020,
+        items: [
+          {
+            month: 'Apr',
+            day: 1,
+            title: 'Joined ??? Inc.',
+            description: '内容',
+          },
+          {
+            month: 'Mar',
+            day: 19,
+            title: 'Graduated from Chiba College of Information and Accounting',
+            description: '内容',
+          },
+        ],
+      },
+    ],
+  },
+}
---
+++ src/components/organisms/CareerTimeline/index.tsx
@@ -0,0 +1,58 @@
+import styles from './index.module.scss'
+
+interface TimelineItemProps {
+  month: string
+  day: number
+  title: string
+  description: string
+}
+
+const TimelineItem = ({
+  month,
+  day,
+  title,
+  description,
+}: TimelineItemProps) => {
+  return (
+    <div className={styles.timeline_item}>
+      <div className={styles.time}>
+        <span className={styles.time_day}>{day}</span>
+        <span className={styles.time_month}>{month}</span>
+      </div>
+      <div className={styles.desc}>
+        <p className={styles.flag}>{title}</p>
+        {description}
+      </div>
+    </div>
+  )
+}
+
+interface TimelineGroupProps {
+  year: number
+  items: Array<TimelineItemProps>
+}
+
+const TimelineGroup = ({ year, items }: TimelineGroupProps) => {
+  return (
+    <div className={styles.timeline_group}>
+      <span className={styles.time_year}>{year}</span>
+      {items.map((item: TimelineItemProps) => {
+        return <TimelineItem key={`${item.month}-${item.day}`} {...item} />
+      })}
+    </div>
+  )
+}
+
+export interface CareerTimelineProps {
+  careers: Array<TimelineGroupProps>
+}
+
+export const CareerTimeline = ({ careers }: CareerTimelineProps) => {
+  return (
+    <div className={styles.cp_timeline03}>
+      {careers.map((career: TimelineGroupProps) => (
+        <TimelineGroup key={career.year} {...career} />
+      ))}
+    </div>
+  )
+}
